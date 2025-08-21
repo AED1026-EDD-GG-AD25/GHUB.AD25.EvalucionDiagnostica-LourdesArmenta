da3370355f88b2bd9ee7199be535a63415a7c369
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-08-21 16:57:17 +0000

    creacion de clase Libro

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,11 +1,9 @@
 package miPrincipal;
 
 public class Principal {
-    public String getGreeting() {
-        return "Hola Mundo!";
-    }
+    
 
     public static void main(String[] args) {
-        System.out.println(new Principal().getGreeting());
+        
     }
 }
