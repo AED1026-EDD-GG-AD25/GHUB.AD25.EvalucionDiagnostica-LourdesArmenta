ec9530cbbed80e8aa5fe3bf29a141659d0722ceb
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-08-22 17:00:30 +0000

    creando la clase InventarioLibros

---
+++ miPrincipal/InventarioLibros.java
@@ -0,0 +1,23 @@
+package miPrincipal;
+public class InventarioLibros {
+    int []numeros;
+
+    numeros = new int[10];
+
+    String []cadenas = new String[50];
+    Libro listaLibros[] = new listaLibros[100];
+
+    //constructor
+    //metodos
+    public void agregar(Libro libro){
+
+    }
+    public void mostrar(){
+
+    }
+    public Libro buscar(String autor){
+       return null;
+
+    }
+    
+}
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,9 +1,38 @@
 package miPrincipal;
+import java.util.Scanner;
 
 public class Principal {
-    
-
+    static Scanner entrada = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        int opc;
+        do{
+            menu();
+            opc = entrada.nextInt();
+            switch (opc) {
+                case 0:
+                    System.out.println("Hasta Luego");
+                    
+                    break;
+                case 1:
+                    break;
+                case 2:
+                   break;
+                case 3:
+                   break;
+            
+                default:
+                    System.out.println("Opcion inválida intente de nuevo!");
+                    break;
+            }
+        } while (opc != 0); 
+    }
+    public static void menu(){
+        System.out.println("MENU PRINCIPAL");
+        System.out.println("1. Agregar");
+        System.out.println("2. Buscar por Autor:");
+        System.out.println("3. Mostrar Todos");
+        System.out.println();
+        System.out.println("0. Salir");
+        System.out.print("\tSelecciona la opción deseada:");
     }
 }
