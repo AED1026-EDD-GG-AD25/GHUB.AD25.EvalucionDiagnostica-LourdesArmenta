1eb8ba88cc23db56f5add2f0209277195dfa3887
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-08-26 16:54:08 +0000

    solucion final

--- miPrincipal/InventarioLibros.java
+++ miPrincipal/InventarioLibros.java
@@ -1,21 +1,38 @@
 package miPrincipal;
 public class InventarioLibros {
-    
     Libro listaLibros[];
-
+    int contador;
     //constructor
     public InventarioLibros(){
-        listaLibros = new Libro[100];
-
+        listaLibros = new Libro[2];
+        contador = 0;
     }
     //metodos
     public void agregar(Libro libro){
-
+        if (contador>=listaLibros.length){
+            //arreglo esta lleno
+            //amplio el arreglo
+            Libro []listaLibrosTemp = new Libro[listaLibros.length*2];
+            //copio el arreglo original al temporal
+            for(int i=0; i<listaLibros.length;i++){
+                listaLibrosTemp[i]=listaLibros[i];
+            }
+            //regreso todo al arreglo original
+            listaLibros = listaLibrosTemp;
+        }
+        listaLibros[contador++] = libro;
     }
     public void mostrar(){
+        for (int i=00; i<contador;i++){
+            System.out.println(listaLibros[i]);
+        }
 
     }
     public Libro buscar(String autor){
+        for(int i=0;i<contador;i++){
+            if(listaLibros[i].getAutor().equalsIgnoreCase(autor))
+               return listaLibros[i];
+        }
        return null;
 
     }
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -4,20 +4,45 @@ import java.util.Scanner;
 public class Principal {
     static Scanner entrada = new Scanner(System.in);
     public static void main(String[] args) {
+        String titulo,autor;
+        int anio;
         int opc;
+        InventarioLibros inventarioLibros = new InventarioLibros();
+
         do{
             menu();
             opc = entrada.nextInt();
+            entrada.nextLine(); //limpiar el buffer del teclado
             switch (opc) {
                 case 0:
                     System.out.println("Hasta Luego");
                     
                     break;
                 case 1:
+                    System.out.print("Proporciona Titulo:");
+                    titulo = entrada.nextLine();
+                    System.out.print("Proporciona Autor:");
+                    autor = entrada.nextLine();
+                    System.out.print("Proporciona Año de publicación:");
+                    anio = entrada.nextInt();
+                    Libro libro = new Libro(titulo,autor,anio);
+                    inventarioLibros.agregar(libro);
+                    System.out.println("Operación Exitosa, el libro se ha agregado");
                     break;
                 case 2:
+                    System.out.print("Proporciona Autor:");
+                    autor = entrada.nextLine();
+                    Libro librobuscado;
+                    librobuscado = inventarioLibros.buscar(autor);
+                    if(librobuscado != null){
+                        System.out.println("Libro Encontrado: "+librobuscado.toString());
+                    }else{
+                        System.out.println("Libro No encontrado!!!!");
+                    }
                    break;
                 case 3:
+                    System.out.println("Listado de Libros:");
+                    inventarioLibros.mostrar();
                    break;
             
                 default:
